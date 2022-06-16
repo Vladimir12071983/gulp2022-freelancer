b613a80d92d41ebf14c63ca1a4d3d8e87933249d
Vladimir <vgura69@gmail.com>
2022-06-16 19:03:16 +0300

    .ibg

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,16 +1,30 @@
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
 export function isWebp() {
-	// Проверка поддержки webp
-	function testWebP(callback) {
-		let webP = new Image();
-		webP.onload = webP.onerror = function () {
-			callback(webP.height == 2);
-		};
-		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-	}
-	// Добавление класса _webp или _no-webp для HTML
-	testWebP(function (support) {
-		let className = support === true ? 'webp' : 'no-webp';
-		document.documentElement.classList.add(className);
-	});
+  // Проверка поддержки webp
+  function testWebP(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src =
+      "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  }
+  // Добавление класса _webp или _no-webp для HTML
+  testWebP(function (support) {
+    let className = support === true ? "webp" : "no-webp";
+    document.documentElement.classList.add(className);
+  });
 }
+
+// Background по классу ibg в html
+function ibg() {
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    }
+  }
+}
+
+ibg();
